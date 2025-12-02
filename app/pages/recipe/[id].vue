ffbd373dbeb7d31f015c34a45fc7e22dd6452d4e
noelie <noelie.talhouarn@edu.univ-fcomte.fr>
2025-12-02 09:16:56 +0100

    feat(recette) add propriété pour la page recette

--- app/pages/recipe/[id].vue
+++ app/pages/recipe/[id].vue
@@ -17,9 +17,19 @@ if (!recipe.value || error.value) throw new Error('Recipe not found')
 <template>
   <div v-if="recipe">
     <h1>{{ recipe.title }}</h1>
-    <p>{{ recipe.instructions }}</p>
+    <p>{{ recipe.description }}</p>
+    <img v-if="recipe.image_url" :src="recipe.image_url" :alt="recipe.title || 'Recipe image'">
+    <p>{{ recipe.cuisine_name }}</p>
+    <p>{{ recipe.goal_name }}</p>
+    <p>{{ recipe.diet_name }}</p>
+    <p>{{ recipe.allergy_name }}</p>
     <ul>
+      <p>Ingredients</p>
       <li v-for="(ingredient, index) in recipe.ingredients" :key="index">{{ ingredient.quantity }} {{ ingredient.unit }} de {{ ingredient.name }}</li>
+    </ul>
+    <ul>
+      <p>instructions</p>
+      <li v-for="(instruction, index) in recipe.instructions" :key="index">{{ instruction.step_number }} {{ instruction.description }} </li>
     </ul>
   </div>
 </template>
