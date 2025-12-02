8edbe48cf71b5404330ecfbbfe011fdb20a7a8cf
noelie <noelie.talhouarn@edu.univ-fcomte.fr>
2025-12-02 08:50:21 +0100

    feta(recette) amélioration types

--- app/pages/recipe/[id].vue
+++ app/pages/recipe/[id].vue
@@ -4,7 +4,7 @@ const route = useRoute()
 const config = useRuntimeConfig()
 
 const { data: recipe, error } = await useAsyncData(`recipe-${route.params.id}`, async () => {
-  const { data } = await $fetch<{ data: fullRecipe }>(`${config.public.apiUrl}/api/recipes/${route.params.id}`
+  const { data } = await $fetch<ApiResponse<fullRecipe>>(`${config.public.apiUrl}/api/recipes/${route.params.id}`
   )
   return data
 })
