085c4b0c91d36f0dc3f549bcfb445fbb32f4c750
noelie <noelie.talhouarn@edu.univ-fcomte.fr>
2025-12-02 14:20:23 +0100

    feat(recipes-grid) filters + barre de recherche

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,40 +1,78 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
-const { data: recipes, error } = await useAsyncData('recipes', async () => {
-  const { data } = await $fetch<ApiResponse<Recipe[]>>(`${config.public.apiUrl}/api/recipes`
-  )
-  return data
-})
 
-
-const { data: cuisines } = await useAsyncData('cuisines', async () => {
-  const { data } = await $fetch<ApiResponse<Cuisine[]>>(`${config.public.apiUrl}/api/cuisines`
-  )
-  return data
-})
+const [{ data: recipes, error }, { data: cuisines }] = await Promise.all([
+  useAsyncData('recipes', async () => {
+    const { data } = await $fetch<ApiResponse<Recipe[]>>(
+      `${config.public.apiUrl}/api/recipes`
+    )
+    return data
+  }),
+  useAsyncData('cuisines', async () => {
+    const { data } = await $fetch<ApiResponse<Cuisine[]>>(
+      `${config.public.apiUrl}/api/cuisines`
+    )
+    return data
+  })
+])
 
 if (error && error.value) throw new Error('Page not found')
+
+const page = ref(1)
+const RECIPES_PER_PAGE = 2
+
+const search = ref('')
 
 const filters = ref<Cuisine['name'][]>([])
 
 function onCheckboxInput ($event: Event) {
   const target = $event.target
   if (!(target instanceof HTMLInputElement)) return
+  page.value = 1
   const value = target.value
   if (!filters.value.includes(value)) {
     filters.value.push(value)
   } else {
-    const index = filters.value.findIndex(filterValue => filterValue === value)
+    const index = filters.value.findIndex(
+      (filterValue) => filterValue === value
+    )
     filters.value.splice(index, 1)
   }
 }
 
 const filteredRecipes = computed<Recipe[]>(() => {
   if (!recipes.value) return []
-  if (!filters.value.length) return recipes.value
-  return recipes.value.filter(recipe => filters.value.includes(recipe.cuisine_name))
+
+  let results = recipes.value
+
+  if (filters.value && filters.value.length) {
+    results = results.filter(recipe => filters.value.includes(recipe.cuisine_name))
+  }
+ 
+  if (search.value.length) {
+    results = results.filter(recipe => {
+      return recipe.title.toLowerCase().includes(search.value.toLocaleLowerCase())
+    })
+  }
+  return results
 })
 
+
+const totalPages = computed(() => {
+  return Math.ceil(filteredRecipes.value.length / RECIPES_PER_PAGE)
+})
+
+const displayRecipes = computed<Recipe[]>(() => {
+  if (!filteredRecipes.value) return []
+  return filteredRecipes.value.slice(
+    (page.value - 1) * RECIPES_PER_PAGE,
+    page.value * RECIPES_PER_PAGE
+  )
+})
+
+function onPageClick (index: number) {
+  page.value = index
+}
 </script>
 
 <template>
@@ -70,17 +108,36 @@ const filteredRecipes = computed<Recipe[]>(() => {
       details="40 MIN • EASY PREP • 3 SERVES"
       button-text="voir la recette"
     />
-    <p>active filter : {{ filters }}</p>
+    <input v-model="search" type="text">
+    <div class="pages">
+      <span
+        v-for="n in totalPages"
+        :key="`page-${n}`"
+        @click="onPageClick(n)"
+      >{{ n }}</span
+      >
+    </div>
     <div class="recipes-filters">
-      <div v-for="(cuisine, index) in cuisines" :key="index" class="recipes-filters__item">
-        <input :id="cuisine.name" type="checkbox" :value="cuisine.name" @input="onCheckboxInput"><label :for="cuisine.name">{{ cuisine.name }}</label>
-
+      <div
+        v-for="(cuisine, index) in cuisines"
+        :key="index"
+        class="recipes-filters__item"
+      >
+        <input
+          :id="cuisine.name"
+          type="checkbox"
+          :value="cuisine.name"
+          @input="onCheckboxInput"
+        ><label :for="cuisine.name">{{ cuisine.name }}</label>
       </div>
     </div>
     <p>Liste des recettes :</p>
     <ul>
-      <li v-for="(recipe, index) in filteredRecipes" :key="index">
-        <NuxtLink :to="`/recipe/${recipe.recipe_id}`">{{ recipe.title }}</NuxtLink></li>
+      <li v-for="(recipe, index) in displayRecipes" :key="index">
+        <NuxtLink :to="`/recipe/${recipe.recipe_id}`">{{
+          recipe.title
+        }}</NuxtLink>
+      </li>
     </ul>
   </main>
 </template>
--- app/types/api/response.d.ts
+++ app/types/api/response.d.ts
@@ -1,4 +1,5 @@
 type ApiResponse<T> = {
   success: boolean
+  count?: number
   data: T 
 }
